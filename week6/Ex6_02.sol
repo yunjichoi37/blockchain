405f9bee5edef90f56dfed47c9badb5c5f7bbf12
yunjichoi37 <yunji3711@naver.com>
2025-10-17 17:49:03 +0900

    Week6: assert

---
+++ week6/Ex6_02.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 < 0.9.0;
+
+contract Ex6_02 {
+    function runAssert(bool _bool) public pure returns(bool) {
+        assert(_bool);
+        return _bool;
+    }
+
+    function divisionByZero(uint _num1, uint _num2) public pure returns(uint) {
+        return _num1 / _num2;
+    }
+}
