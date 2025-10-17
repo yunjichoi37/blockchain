b4946ffc77779f7979955dd4562c0d0a7bde665e
yunjichoi37 <yunji3711@naver.com>
2025-10-17 16:53:00 +0900

    Week6: inheritance constructor 매개변수

---
+++ week6/inheritance2.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 < 0.9.0;
+
+contract Student {
+    string public schoolName = "The University of Solidity";
+    string public major;
+    constructor(string memory _major) {
+        major = _major;
+    }
+}
+
+contract ArtStudent is Student("Art") {
+}
+contract MusicStudent is Student {
+    constructor() Student("Music") {
+    }
+}
+contract MathStudent is Student {
+    constructor(string memory _major) Student(_major){
+    }
+}
