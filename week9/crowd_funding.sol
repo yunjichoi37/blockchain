b8528109491ace0194bf7c366964cd1ff98cffe5
yunjichoi37 <yunji3711@naver.com>
2025-11-12 20:37:47 +0900

    Week9: assignment2

--- week9/crowd_funding.sol
+++ week9/crowd_funding.sol
@@ -7,10 +7,8 @@ contract CrowdFunding {
         uint amount;
     }
 
-    mapping (uint => Investor) public investors;
-    // mapping (address => uint) public investors;
-
-    event Fund(address _address, uint _value);
+    mapping (uint => Investor) public investors; // 첫 번째 투자자: index = 1로 설정할 것이다.
+    event Funded(address _address, uint _value); // fund()가 호출될 때 발생할 event
 
     address public owner;
     uint public numInvestors;
@@ -20,58 +18,64 @@ contract CrowdFunding {
     uint public goalAmount;
     uint public totalAmount;
 
-    modifier onlyOwner() {
+    modifier onlyOwner() { // 소유자만 실행할 수 있게 하는 modifier
         require(owner == msg.sender, "You are not the owner.");
         _;
     }
 
     constructor(uint _duration, uint _goalAmount) {
-        owner = msg.sender;
+        owner = msg.sender; // 생성시 설정되게 만들기
 
-        deadline = block.timestamp + _duration;
-        goalAmount = _goalAmount * 1 ether;
-        status = "Funding";
-        ended = false;
+        deadline = block.timestamp + _duration; // deadline = 현 시간 + 모금 진행 기간
+        goalAmount = _goalAmount * 1 ether; // _goalAmount는 wei 단위라 1 ether를 곱해준다.
+        status = "Funding"; // 초기 상태를 Funding으로 설정해준다.
+        ended = false; // 초기 종료 여부를 false로 설정해준다.
 
-        numInvestors = 0;
-        totalAmount = 0;
+        numInvestors = 0; // 투자자 수를 0으로 초기화한다.
+        totalAmount = 0; // 총 투자액을 0으로 초기화한다.
     }
 
     function fund() public payable {
-        require(msg.value > 0, "Fund amount must be greater than 0.");
-        require(block.timestamp <= deadline, "Funding ended."); // 여기서 ended를 true로 바꿔버릴 수 있음.
+        require(msg.value > 0, "Fund amount must be greater than 0."); // funding 금액은 0보다 커야 한다.
+        require(block.timestamp <= deadline, "Funding ended."); // funding 마감 시간이 지나면 안 된다.
+        
+        // if (block.timestamp > deadline) {
+        //     ended = true;
+        //     revert("Funding ended.");
+        // }
+        // 위처럼 if-revert 조합을 사용하여 마감기간이 끝났을 때 ended를 true로 설정하여 했으나,
+        // revert로 인해 롤백이 발생해 업데이트가 되지 않는다.
+        // 따라서 그냥 require문을 사용하였다.
 
-        totalAmount += msg.value;
-        numInvestors += 1;
+        totalAmount += msg.value; // 투자액을 받은 만큼 더해준다.
+        numInvestors += 1; // 투자자의 수를 1만큼 늘려준다.
         // investors[msg.sender] += msg.value;
-        investors[numInvestors].addr = msg.sender;
-        investors[numInvestors].amount = msg.value;
-        emit Fund(msg.sender, msg.value);
+        investors[numInvestors].addr = msg.sender; // n번째 투자자의 주소를 업데이트 한다.
+        investors[numInvestors].amount = msg.value; // n번째 투자자의 투자액을 업데이트 한다.
+        emit Funded(msg.sender, msg.value); // 누가 얼마만큼 보냈는지 event를 발생시킨다.
     }
 
     function checkGoalReached() public onlyOwner {
-        require(deadline < block.timestamp, "not yet deadline");
+        require(deadline < block.timestamp, "not yet deadline");  // 마감일이 도달하지 않았다고 알려준다.
 
-        if (totalAmount < goalAmount) {
-            for (uint i = 1; i <= numInvestors; i++) {
-                payable(investors[i].addr).transfer(investors[i].amount);
+        if (totalAmount < goalAmount) { // 모인 금액이 목표 금액보다 적으면,
+            for (uint i = 1; i <= numInvestors; i++) { // 투자자들에게 자기가 투자한 금액만큼을 돌려준다.
+                payable(investors[i].addr).transfer(investors[i].amount); // 이때 payable, transfer을 사용한다.
             }
-            status = "Campaign Failed";
-            ended = true;
-        } else { // 펀드 성공 시 owner에게 전체 금액 보내주기
-            payable(owner).transfer(totalAmount);
-            status = "Campaign Succeeded";
-            ended = true;
+            status = "Campaign Failed"; // 투자 실패로 상태 업데이트
+            // ended = true;
+        } else { // 펀드 성공 시 
+            payable(owner).transfer(totalAmount); // owner에게 전체 금액 보내주기
+            status = "Campaign Succeeded"; // 투자 성공으로 상태 업데이트
+            // ended = true;
         }
+        ended = true; // deadline이 지났으니 true로 설정해준다.
     }
 
-    function getInvestors() public returns(Investor memory) {
-        //return investors[];
-        // for (uint i = 1; i <= numInvestors; i++) {
-        //     return investors[i];
-        // }
+    function getInvestors() public view returns(address[] memory) {
+        address[] memory investorAddr = new address[](numInvestors); // investor의 주소들을 담을 address[]를 초기화해준다.
+        for (uint i = 1; i <= numInvestors; i++) 
+            investorAddr[i - 1] = investors[i].addr; // 여기서는 다시 1만큼 빼주어 저장한다. n번째 투자자를 찾는 게 아니라 단순 나열이 목적이기 때문이다.
+        return investorAddr; // 반환해준다.
     }
-
 }
-
-// 시간이 끝나서 자동 종료되는 건 못 하나?
--- week9/registerContract.sol
+++ week9/registerContract.sol
@@ -3,70 +3,74 @@ pragma solidity >=0.7.0 < 0.9.0;
 
 contract NameRegistry {
     struct ContractInfo {
-        address contractOwner;
-        address contractAddress;
-        string description;
+        address contractOwner; // 컨트랙트 소유자 주소
+        address contractAddress; // 컨트랙트 주소
+        string description; // 컨트랙트에 대한 설명
     }
 
     uint public numContracts;
-    string[] contractNames;
 
-    mapping (string => ContractInfo) public registeredContracts;
-    
-    event ContractRegistered(string _name);
-    event ContractDeleted(string _name);
-    event ContractUpdated(string _name, string _what);
+    mapping (string => ContractInfo) public registeredContracts; // 컨트랙트 name으로 mapping하기
+        
+    event ContractRegistered(string _name); // contract가 등록됨을 알리는 event
+    event ContractDeleted(string _name); // contract가 삭제됨을 알리는 event
+    event ContractUpdated(string _name, string _what); // contract가 변경됨을 알리는 event
 
-    modifier onlyOwner(string memory _name) {
+    modifier onlyOwner(string memory _name) { // _name이라는 contract의 소유자만 접근할 수 있도록 제한하는 modifier
         require(registeredContracts[_name].contractOwner == msg.sender, "You are not the owner of this contract.");
         _;
     }
 
     constructor() {
-        numContracts = 0;
+        numContracts = 0; // 등록된 contract의 수를 0으로 초기화한다.
     }
 
+    // 아직 등록되지 않은 contract라면 새로 등록해주는 함수이다.
     function registerContract(string memory _name, address _contractAddress, string memory _description) public {
+        // 이미 등록되었는지 확인하는 기준은 _name이다. _name에 해당하는 주소가 NULL이 아니면 contract가 존재한다는 뜻이기 때문에 require
         require(registeredContracts[_name].contractAddress == address(0), "this name is already exist.");
-        registeredContracts[_name] = ContractInfo(msg.sender, _contractAddress, _description);
-        emit ContractRegistered(_name);
+        registeredContracts[_name] = ContractInfo(msg.sender, _contractAddress, _description); // mapping을 이용해 등록해준다.
+        numContracts += 1; // 등록된 contract 수를 1 올려준다.
+        emit ContractRegistered(_name); // _name이라는 contract가 등록되었다고 event를 발생시킨다.
     }
 
+    // 등록된 contract를 삭제하는 함수이다. 소유자만 가능하다.
     function unregisterContract(string memory _name) public onlyOwner(_name) {
         // require(registeredContracts[_name].contractAddress != address(0), "this name is not exist.");
-        delete(registeredContracts[_name]);
-        emit ContractDeleted(_name);
+        // 위 코드가 없어도 되는 이유는, modifier에서 _name에 해당하는 소유자를 검증해주기 때문이다.
+        delete(registeredContracts[_name]); // delete를 사용해 없애준다.
+        numContracts -= 1; // 등록된 contract 수를 1 내려준다.
+        emit ContractDeleted(_name); // _name이라는 contract가 삭제되었다고 event를 발생시킨다.
     }
 
+     // contract의 소유자를 변경하는 함수이다. 소유자만 가능하다.
     function changeOwner(string memory _name, address _newOwner) public onlyOwner(_name) {
-        require(_newOwner != address(0), "New Owner's Address is NULL!!!");
-        registeredContracts[_name].contractOwner = _newOwner;
-        emit ContractUpdated(_name, "Contract Owner");
+        require(_newOwner != address(0), "New Owner's Address is NULL!!!"); // new Owner가 null인 경우를 방지해준다.
+        registeredContracts[_name].contractOwner = _newOwner; // 소유자를 변경해준다.
+        emit ContractUpdated(_name, "Contract Owner"); // _name의 소유자가 바뀌었음을 "Contract Owner"라는 메시지로 알려준다.
     }
 
     function getOwner(string memory _name) public view returns(address) {
-        return registeredContracts[_name].contractOwner;
+        return registeredContracts[_name].contractOwner; // 컨트랙트 소유자의 정보를 확인한다.
     }
 
+    // contract의 address를 변경하는, 소유자만 사용할 수 있는 함수이다.
     function setAddr(string memory _name, address _addr) public onlyOwner(_name) {
-        registeredContracts[_name].contractAddress = _addr;
-        emit ContractUpdated(_name, "Contract Address");
+        registeredContracts[_name].contractAddress = _addr; // 주소를 변경해준다.
+        emit ContractUpdated(_name, "Contract Address"); // _name의 주소가 바뀌었음을 "Contract Address"라는 메시지로 알려준다.
     }
 
     function getAddr(string memory _name) public view returns(address) {
-        return registeredContracts[_name].contractAddress;
+        return registeredContracts[_name].contractAddress; // 컨트랙트 주소 정보를 확인한다.
     }
 
+    // contract의 설명을 변경하는 함수이다. 소유자만 가능하다.
     function setDescription(string memory _name, string memory _description) public onlyOwner(_name) {
-        registeredContracts[_name].description = _description;
-        emit ContractUpdated(_name, "Contract Description");
+        registeredContracts[_name].description = _description; // 설명을 변경해준다.
+        emit ContractUpdated(_name, "Contract Description"); // _name의 설명이 바뀌었음을 "Contract Description"이라는 메시지로 알려준다.
     }
 
     function getDescription(string memory _name) public view returns(string memory) {
-        return registeredContracts[_name].description;
+        return registeredContracts[_name].description; // 컨트랙트 설명을 확인한다.
     }
-
-    // function getAllContractNames() public returns(string[] memory) {
-    //     for 
-    // }
 }
