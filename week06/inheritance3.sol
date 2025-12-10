6b04962cc6e8d7dde8cf2a4d83db143ef11c6698
yunjichoi37 <yunji3711@naver.com>
2025-12-10 15:22:53 +0900

    Week6: inheritance private/internal

---
+++ week06/inheritance3.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 < 0.9.0;
+
+contract Student {
+    string private schoolName = "Solidity University";
+    string internal schoolNumbers = "02-705-2123";
+}
+
+contract ArtStudent is Student() {
+    // function getSchoolName() public view returns(string memory) {
+    //     return schoolName;
+    // }
+
+    function getSchoolNumbers() public view returns(string memory) {
+        return schoolNumbers;
+    }
+}
