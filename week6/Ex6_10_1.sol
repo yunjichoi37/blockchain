042bda1300a625e175809eb09f7f7dedcee1b861
yunjichoi37 <yunji3711@naver.com>
2025-12-10 15:24:37 +0900

    파일 경로 변경

--- week6/Ex6_10_1.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 < 0.9.0;
-
-contract ArtStudent {
-    string public schoolName = "The Solidity University";
-    uint public schoolHours = 5;
-}
